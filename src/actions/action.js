e8030ded8d3b6e62f6bf2715efabb8c64635234f
MobGG <tammanoon.k@sahapat.co.th>
2020-03-20 10:38:38 +0700

    Add history actions

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,23 +1,38 @@
-let nextTodoId = 0;
+let nextTodoId = 1;
 
 export const create = (text) => ({
     type: 'CREATE',
     id: nextTodoId++,
     text
-})
+});
 
 export const remove = (id) => ({
     type: 'REMOVE',
     id
-})
+});
 
 export const search = (text) => ({
     type: 'SEARCH',
     text
-})
+});
+
+export const writeHistory = (text, removed) => ({
+    type: 'WRITE_HISTORY',
+    text,
+    removed
+});
+
+export const clearAllHistory = () => ({
+    type: 'CLEAR_ALL_HISTORY'
+});
+
+export const sort = (text) => ({
+    type: 'SORT',
+    text
+});
 
 // export const VisibilityFilters = {
 //     SHOW_ALL: 'SHOW_ALL',
 //     SHOW_COMPLETED: 'SHOW_COMPLETED',
 //     SHOW_ACTIVE: 'SHOW_ACTIVE'
-// }
+// };
